80aba821ea549fb1a1f354f67bc7753e5c4cb56f
冯遥 <250990918@qq.com>
2017-07-19 09:26:23 +0800

    Create MySolution.java

---
+++ others/MySolution.java
@@ -0,0 +1,5 @@
+1 Describe MVP
+MVP架构背后的核心思想就是将Activity/Fragment变成一个单纯的View，负责展示数据并将各种事件分发给中间人，也就是Presenter。
+Presenter会处理每一个事件，从Model层获取或上传数据，并将获得的数据进行处理并让View层展示。
+Presenter与View（这里是Activity/Fragment）的通信，是通过Activity/Fragment所继承的View接口来间接完成的。
+这样数据展示过程的任务就分开了，并使测试更加方便，因为Presenter不包含任何依赖Android的代码。
